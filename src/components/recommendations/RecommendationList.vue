bdebd01bb114455b849d7a28e344c41e77ca50eb
Andres Eloy Fernandes <andreseloyfernandes@gmail.com>
2022-02-27 23:10:22 -0400

    Added aria placeholders to all of the search bars

--- src/components/recommendations/RecommendationList.vue
+++ src/components/recommendations/RecommendationList.vue
@@ -35,6 +35,7 @@
           clearable
           single-line
           hide-details
+          :aria-placeholder="$t('global.search_bar_accessibility')"
         ></v-text-field>
       </v-col>
     </v-row>
