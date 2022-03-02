6e28ef8b30eb80e41c0a8796900dda8d556c2bad
belgrades <fcremaldc@gmail.com>
2022-03-02 01:08:43 +0100

    Adding aria label to report view

--- src/views/reports/active reports/ReportView.vue
+++ src/views/reports/active reports/ReportView.vue
@@ -23,6 +23,7 @@
                 color="primary"
                 v-bind="attrs"
                 v-on="on"
+                :aria-label="$t('reports.to_add_info')"
                 @click="hide_left_column = !hide_left_column"
               >
                 <v-icon v-if="!hide_left_column" class="pr-2"
@@ -32,27 +33,25 @@
                 {{ $t("reports.to_add") }}
               </v-btn>
             </template>
-            <span>{{ $t("reports.to_add_info") }} </span>
+            <span> {{ $t("reports.to_add_info") }} </span>
           </v-tooltip>
         </v-card>
       </v-col>
       <v-col cols="auto" align-self="center">
-        <v-card class="pa-2" elevation="0">
-          <v-tooltip bottom close-delay="100">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                medium
-                color="primary"
-                v-bind="attrs"
-                v-on="on"
-                @click="overlay_export = true"
-                >{{ $t("reports.export") }}</v-btn
-              >
-            </template>
-
-            <span>{{ $t("reports.export_info") }} </span>
-          </v-tooltip>
-        </v-card>
+        <v-tooltip bottom close-delay="100">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              medium
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+              :aria-label="$t('reports.export_info')"
+              @click="overlay_export = true"
+              >{{ $t("reports.export") }}</v-btn
+            >
+          </template>
+          <span>{{ $t("reports.export_info") }}</span>
+        </v-tooltip>
       </v-col>
     </v-row>
     <v-row justify="center">
