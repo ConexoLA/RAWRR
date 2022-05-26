c1aba09fa34f36c1d0b0985b86f5c5c30b2184a4
Fabian Ziffzer <fabianziffzer@gmail.com>
2022-05-26 17:20:12 -0400

    Added buttons and dialogs to delete a t_history

--- src/components/threats/ThreatHistory.vue
+++ src/components/threats/ThreatHistory.vue
@@ -7,6 +7,17 @@
       </v-btn>
     </router-link>
 
+    <v-spacer></v-spacer>
+
+    <v-btn
+      color="error"
+      v-if="audits.length > 1"
+      @click="dialogDeleteAll = true"
+    >
+      <v-icon class="mr-2">mdi-delete-variant</v-icon>
+      {{ $t("threats.threat_history.delete") }}
+    </v-btn>
+
     <v-row align="center" justify="center">
       <v-col cols="auto" class="text-center">
         <v-card class="elevation-2 text-left">
@@ -79,8 +90,13 @@
                 class="text-h5 red darken-2"
                 v-if="n.type[0] == 1"
                 :tabIndex="n.type[1]"
-                >{{ $t("threats.modified_threat") }}</v-card-title
               >
+                {{ $t("threats.modified_threat") }}
+                <v-spacer></v-spacer>
+                <v-btn icon @click="dialogDeleteSingle = true">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-card-title>
               <v-card-title
                 class="text-h5 red darken-2"
                 v-if="n.type[0] == 0"
@@ -333,7 +349,7 @@
                   <div v-if="!n.asset_name_new && n.asset_name_old">
                     <div :tabIndex="n.asset_name_old[1]">
                       <b>{{ $t("threats.deleted_asset") }}: </b>
-                      <strike>{{ n.asset_name_old }}</strike>
+                      <strike>{{ n.asset_name_old[0] }}</strike>
                     </div>
                   </div>
                 </div>
@@ -376,6 +392,52 @@
         </v-card-actions>
       </v-card>
     </v-overlay>
+    <v-dialog v-model="dialogDeleteSingle" width="500">
+      <v-card>
+        <v-card-title class="text-h5 warning">
+          {{ $t("global.delete_confirm") }}
+        </v-card-title>
+
+        <br />
+
+        <v-card-text>
+          {{ $t("threats.threat_history.confirm_delete") }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="accent" text @click="dialogDeleteSingle = false">
+            {{ $t("global.cancel") }}
+          </v-btn>
+          <v-btn color="error" text @click="yourFunctionHere()">
+            {{ $t("global.delete") }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialogDeleteAll" width="500">
+      <v-card>
+        <v-card-title class="text-h5 warning">
+          {{ $t("global.delete_confirm") }}
+        </v-card-title>
+
+        <br />
+
+        <v-card-text>
+          {{ $t("threats.threat_history.confirm_delete_all") }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="accent" text @click="dialogDeleteAll = false">
+            {{ $t("global.cancel") }}
+          </v-btn>
+          <v-btn color="error" text @click="yourFunctionHere()">
+            {{ $t("global.delete") }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -385,6 +447,8 @@ import { mapGetters } from "vuex";
 export default {
   props: ["threat", "audits"],
   data: () => ({
+    dialogDeleteSingle: false,
+    dialogDeleteAll: false,
     overlay_observation: false,
     description_value: "",
     description_title: "",
